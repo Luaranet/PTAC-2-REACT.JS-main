1beada1d244d537f3e3d6004bd096db4f1b64a0f
Luaranet <luaranet08@gmail.com>
2023-11-17 14:33:29 -0400

    commit

--- src/Detalhe/detalhe.jsx
+++ src/Detalhe/detalhe.jsx
@@ -1,12 +1,22 @@
 import { useParams } from "react-router-dom";
+import Card from "../Componentes/Card";
 
 export default function Detalhe(){
+
     const { id } = useParams();
+    const lista = JSON.parse( localStorage.getItem("Lista"));
+
+    lista.filter( (objeto) => {
+        if(objeto.id == id) {
+            return objeto;
+        }
+        return null;
+    })
+
+    console.log( atividade[0] );
 
     return(
-        <div>
-            <h1>olá {id}</h1>
-        </div>
+        <Card atividade={atividade[0]} />
     );
 
 
